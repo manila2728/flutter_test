12390af70f36911bdcc2a5678f07a0b8ac4203b5
M.Z. <48641307+manila2728@users.noreply.github.com>
2024-09-05 13:31:55 +0800

    1
    1

---
+++ conduit_server/bin/main.dart
@@ -0,0 +1,12 @@
+import 'package:conduit_server/conduit_server.dart';
+
+Future main() async {
+  final app = Application<ConduitServerChannel>()
+    ..options.configurationFilePath = "config.yaml"
+    ..options.port = 8888;
+
+  await app.startOnCurrentIsolate();
+
+  print("Application started on port: ${app.options.port}.");
+  print("Use Ctrl-C (SIGINT) to stop running the application.");
+}
---
+++ conduit_server/lib/conduit_server.dart
@@ -0,0 +1,11 @@
+/// conduit_server
+///
+/// A conduit web server.
+library conduit_server;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:conduit_core/conduit_core.dart';
+
+export 'channel.dart';
